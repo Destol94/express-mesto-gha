bd892ebf82cd70a1579168d88ffda496d3971518
Destol94 <destol94@gmail.com>
2022-12-11 23:04:21 +0700

    13ПР 1 итерация исправления

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,6 @@ router.get('/', getUsers);
 router.get('/:userId', getUser);
 router.post('/', createUser);
 router.patch('/me', updateProfile);
-router.patch('me/avatar', updateAvatar);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
