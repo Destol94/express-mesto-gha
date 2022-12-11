e1e696dd9f7d8b731072c4cda8c6478fb600af30
Destol94 <destol94@gmail.com>
2022-12-11 21:23:26 +0700

    13ПР 1 итерация испраления

--- controllers/users.js
+++ controllers/users.js
@@ -37,7 +37,7 @@ const createUser = async (req, res) => {
   const { name, about, avatar } = req.body;
   try {
     const user = await User.create({ name, about, avatar });
-    return res.status(201).json(user);
+    return res.status(201).json({message: `${res.message}`});
   }
   catch (err) {
     validationErrorHandler(err, res);
@@ -51,7 +51,7 @@ const updateProfile = async (req, res) => {
       id,
       { name, about }
     )
-    return res.status(201).json(user);
+    return res.status(200).json({message: `${res.message}`});
   }
   catch (err) {
     validationErrorHandler(err, res);
