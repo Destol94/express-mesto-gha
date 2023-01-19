45a14ab95c4226f7107c276ed9a67e8e6039a920
Destol94 <destol94@gmail.com>
2023-01-20 00:03:25 +0700

    15ПР корс

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 
 app.use(requestLogger);
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/users', checkAuth, routerUser);
 // app.get('/crash-test', () => {
--- utils/corsUtils.js
+++ utils/corsUtils.js
@@ -1,6 +1,8 @@
 const allowedCors = [
   'https://project-mesto.nomoredomains.club',
   'http://project-mesto.nomoredomains.club',
+  'https://api.project-mesto.nomoredomains.club',
+  'http://api.project-mesto.nomoredomains.club',
   'http://localhost:3000',
   'http://localhost:3001',
 ];
